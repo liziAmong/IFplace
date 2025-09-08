d27033f7fa35b8bbdc518beea89bdc1cc87093e3
liziAmong <20light02@gmail.com>
2025-09-08 23:24:49 +0900

    Create data.js

---
+++ data.js
@@ -0,0 +1,24 @@
+const artworks = [
+  {
+    id: 1,
+    title: "작품 1",
+    link: "https://wplace.net/example1",
+    tags: ["#리쟈", "#하유"],
+    hints: [
+      "00시 주변에 있습니다.",
+      "00시에서 3시 방향으로 가면 있어요!",
+      "작품의 위치를 확인해보세요!"
+    ]
+  },
+  {
+    id: 2,
+    title: "작품 2",
+    link: "https://wplace.net/example2",
+    tags: ["#민수"],
+    hints: [
+      "06시 근처에 있습니다.",
+      "06시에서 9시 방향으로 가면 있어요!",
+      "작품의 위치를 확인해보세요!"
+    ]
+  }
+];
